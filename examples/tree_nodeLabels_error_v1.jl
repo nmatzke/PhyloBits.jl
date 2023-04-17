f4ff638fb9b19714ab9e50d3470a62fee9ab23ed
nmatzke <matzke@berkeley.edu>
2023-04-17 13:59:02 +1200

    Create tree_nodeLabels_error_v1.jl

---
+++ examples/tree_nodeLabels_error_v1.jl
@@ -0,0 +1,90 @@
+
+using Test, PhyBEARS, DataFrames
+
+using Dates									# for e.g. Dates.now(), DateTime
+using Distributed						# for e.g. @spawn
+using Combinatorics					# for e.g. combinations()
+using DataFrames						# for DataFrame()
+using DelimitedFiles				# for readdlm()
+using NLopt									# seems to be the best gradient-free, box-constrained								
+
+# List each PhyBEARS code file prefix here
+using PhyloBits.TrUtils			# for e.g. numstxt_to_df()
+using PhyloBits.TreeTable
+using PhyBEARS.BGExample
+using PhyBEARS.StateSpace
+using PhyBEARS.TreePass
+using PhyBEARS.SSEs
+using PhyBEARS.Parsers
+using PhyBEARS.ModelLikes # e.g. setup_DEC_SSE2
+using PhyBEARS.Uppass
+
+"""
+# Run with:
+cd("/Users/nmat471/HD/GitHub/PhyBEARS.jl/sims/sunkNZ_v1/")
+include("/Users/nmat471/HD/GitHub/PhyBEARS.jl/sims/sunkNZ_v1/prt_nodelabel_trees_v1.jl")
+"""
+# 
+# """
+# # Run with:
+# include("/GitHub/PhyBEARS.jl/test/runtests_ClaSSE_tree_n12_DECj.jl")
+# """
+# 
+# @testset "Example" begin
+# 	@test hello("runtests_ClaSSE_tree_n12_DECj.jl") == "runtests_ClaSSE_tree_n12_DECj.jl"
+# #	@test domath(2.0) ≈ 7.0
+# end
+# 
+# 
+# #######################################################
+# # Do a bunch of tests of the SSE calculation of 
+# # Ds, Es, and likelihoods, on
+# # branches, nodes, and trees,
+# # under a variety of simple and more complex models
+# #######################################################
+# 
+#@testset "prt_nodelabel_trees_v1.jl" begin
+# 
+
+
+
+
+
+
+# UndefVarError: node not defined
+
+
+
+
+
+include_null_range = true
+numareas = Rncol(geog_df)-1
+max_range_size = numareas
+n = numstates_from_numareas(numareas, max_range_size, include_null_range)
+
+# DEC-type SSE model on Hawaiian Psychotria
+# We are setting "j" to 0.0, for now -- so, no jump dispersal
+bmo = construct_BioGeoBEARS_model_object();
+#bmo.type[bmo.rownames .== "j"] .= "free";
+bmo.est[bmo.rownames .== "birthRate"] .= ML_yule_birthRate(tr);
+bmo.est[bmo.rownames .== "deathRate"] .= 0.0;
+bmo.est[bmo.rownames .== "d"] .= 0.034;
+bmo.est[bmo.rownames .== "e"] .= 0.028;
+bmo.est[bmo.rownames .== "a"] .= 0.0;
+bmo.est[bmo.rownames .== "j"] .= 0.0;
+bmo.est[bmo.rownames .== "u"] .= 0.0;
+bmo.est[bmo.rownames .== "x"] .= 0.0;
+bmo.est[bmo.rownames .== "xv"] .= 0.1;
+bmo.max[bmo.rownames .== "xv"] .= 10.0;
+
+bmo.est[:] = bmo_updater_v2(bmo, inputs.setup.bmo_rows);
+
+# CHANGE PARAMETERS BEFORE E INTERPOLATOR
+root_age_mult=1.5; max_range_size=NaN; include_null_range=false; max_range_size=NaN
+max_range_size = NaN # replaces any background max_range_size=1
+inputs = setup_DEC_SSE2(numareas, tr, geog_df; root_age_mult=1.5, max_range_size=max_range_size, include_null_range=true, bmo=bmo);
+(setup, res, trdf, bmo, files, solver_options, p_Es_v5, Es_tspan) = inputs;
+
+
+
+# end # END prt_nodelabel_trees_v1.jl
