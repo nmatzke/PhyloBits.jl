fe978cdfd78f6cb7e0c076d3d028e450d6ba4883
Nicholas J. Matzke <nickmatzke.ncse@gmail.com>
2024-03-01 21:35:03 +1300

    ops: open startup.jl

--- src/TrUtils.jl
+++ src/TrUtils.jl
@@ -21,7 +21,7 @@ using UUIDs					# for UUID() object
 
 print("...done.\n")
 
-export hello_world_TrUtils, opd, opf, ls, list_files, offdiag, make_diag_TF, make_offdiag_TF, convert_df_datatypes!, rename_df!, nthreads_procs, get_installed_path, pp, get_pkg_version, get_pkg_status, has, has_name, get_keys_matching_value, get_keys_matching_name, UUIDs_to_string, get_pkg_uuid, merge_paths, mp, merge_path_with_file, mpf, getwd, Rgetwd, setwd, getfn, readtable, numstxt_to_df, recursive_find, include_jls, source, get_a_most_common_value, indexed_Dict_to_DF, convert_is_js_to_single_index, pair_of_indices_to_single_index_column_first, dim, Rdim, seq, Rchoose, R_in, R_in_vv_ints, vv_to_v_ints, Rcbind, Rrbind, Rpaste, Rpaste0, paste, paste0, type, class, Rclass, odds, evens, slashslash, ss, addslash, write_txt, write_txtvec, append_txt, append_txtvec, write_trdf_ancstates, df_to_Rdata, Reval, Rdput, julian_dput, Rnames, rnames, rn, Rtypes, rtypes, compare_dfs, get_max_df_diffs_byCol, subset_vec_of_vecs, subsetvv, subvv, vector_of_vectors_to_df, vvdf, vfft, vec_to_vecvec, ont, Rnrow, Rncol, Rsize, Rorder, headLR, flat2, rowSums, colSums, rowSums_df, colSums_df, single_element_array_to_scalar, headf, moref, get_alphabets, LETTERS, letters, GREEKLETTERS, greekletters, greekletters2, scr2str, lagrange_to_tip, isnan2
+export hello_world_TrUtils, opd, opf, ops, open_startup, opc, ls, list_files, offdiag, make_diag_TF, make_offdiag_TF, convert_df_datatypes!, rename_df!, nthreads_procs, get_installed_path, pp, get_pkg_version, get_pkg_status, has, has_name, get_keys_matching_value, get_keys_matching_name, UUIDs_to_string, get_pkg_uuid, merge_paths, mp, merge_path_with_file, mpf, getwd, Rgetwd, setwd, getfn, readtable, numstxt_to_df, recursive_find, include_jls, source, get_a_most_common_value, indexed_Dict_to_DF, convert_is_js_to_single_index, pair_of_indices_to_single_index_column_first, dim, Rdim, seq, Rchoose, R_in, R_in_vv_ints, vv_to_v_ints, Rcbind, Rrbind, Rpaste, Rpaste0, paste, paste0, type, class, Rclass, odds, evens, slashslash, ss, addslash, write_txt, write_txtvec, append_txt, append_txtvec, write_trdf_ancstates, df_to_Rdata, Reval, Rdput, julian_dput, Rnames, rnames, rn, Rtypes, rtypes, compare_dfs, get_max_df_diffs_byCol, subset_vec_of_vecs, subsetvv, subvv, vector_of_vectors_to_df, vvdf, vfft, vec_to_vecvec, ont, Rnrow, Rncol, Rsize, Rorder, headLR, flat2, rowSums, colSums, rowSums_df, colSums_df, single_element_array_to_scalar, headf, moref, get_alphabets, LETTERS, letters, GREEKLETTERS, greekletters, greekletters2, scr2str, lagrange_to_tip, isnan2
 
 # cutting as it requires the loading of Plots (slow)
 # saveopen, 
@@ -52,17 +52,39 @@ end
 
 
 """
-Run the Terminal command "open ."
+Run the Terminal command "open ~/.julia/config/startup.jl"
 """
-function ls()
-	run(`ls - la`);
+function ops()
+	run(`sh -c "open ~/.julia/config/startup.jl"`)
 end
 
 """
-Run the Terminal command "open ."
+Run the Terminal command "open ~/.julia/config/startup.jl"
+"""
+function open_startup()
+	run(`sh -c "open ~/.julia/config/startup.jl"`)
+end
+
+"""
+Run the Terminal command "open ~/.julia/config/"
+"""
+function opc()
+	run(`sh -c "open ~/.julia/config/"`)
+end
+
+
+"""
+Run the Terminal command "ls ."
+"""
+function ls()
+	run(`ls -la`);
+end
+
+"""
+Run the Terminal command "ls ."
 """
 function list_files()
-	run(`ls - la`);
+	run(`ls -la`);
 end
 
 
